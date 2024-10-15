ed629edf558b35436a92fea12c75b820feb83c75
Khawlaa31 <bouhssinekhawla51@gmail.com>
2024-10-15 19:53:42 +0100

    done

--- Final Project RBK/client/src/pages/LogIn.jsx
+++ Final Project RBK/client/src/pages/LogIn.jsx
@@ -64,50 +64,50 @@ const Login = () => {
   );
 };
 
-const styles = {
-  container: {
-    width: "300px",
-    margin: "100px auto",
-    padding: "20px",
-    background: "#fff",
-    borderRadius: "8px",
-    boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
-  },
-  heading: {
-    textAlign: "center",
-    color: "#333",
-  },
-  inputGroup: {
-    marginBottom: "15px",
-  },
-  label: {
-    display: "block",
-    marginBottom: "5px",
-    color: "#555",
-  },
-  input: {
-    width: "100%",
-    padding: "10px",
-    border: "1px solid #ccc",
-    borderRadius: "4px",
-    transition: "border-color 0.3s",
-  },
-  errorMessage: {
-    color: "red",
-    textAlign: "center",
-    marginTop: "10px",
-  },
-  submitButton: {
-    width: "100%",
-    padding: "10px",
-    backgroundColor: "#007bff",
-    color: "#fff",
-    border: "none",
-    borderRadius: "4px",
-    cursor: "pointer",
-    fontSize: "16px",
-    transition: "background-color 0.3s",
-  },
-};
+// const styles = {
+//   container: {
+//     width: "300px",
+//     margin: "100px auto",
+//     padding: "20px",
+//     background: "#fff",
+//     borderRadius: "8px",
+//     boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+//   },
+//   heading: {
+//     textAlign: "center",
+//     color: "#333",
+//   },
+//   inputGroup: {
+//     marginBottom: "15px",
+//   },
+//   label: {
+//     display: "block",
+//     marginBottom: "5px",
+//     color: "#555",
+//   },
+//   input: {
+//     width: "100%",
+//     padding: "10px",
+//     border: "1px solid #ccc",
+//     borderRadius: "4px",
+//     transition: "border-color 0.3s",
+//   },
+//   errorMessage: {
+//     color: "red",
+//     textAlign: "center",
+//     marginTop: "10px",
+//   },
+//   submitButton: {
+//     width: "100%",
+//     padding: "10px",
+//     backgroundColor: "#007bff",
+//     color: "#fff",
+//     border: "none",
+//     borderRadius: "4px",
+//     cursor: "pointer",
+//     fontSize: "16px",
+//     transition: "background-color 0.3s",
+//   },
+// };
 
 export default Login;
