5773b227ecc0360bb5a7db337d7835e010c06aab
Khawlaa31 <bouhssinekhawla51@gmail.com>
2024-10-16 21:54:13 +0100

    done

--- Final Project RBK/client/src/main.jsx
+++ Final Project RBK/client/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Test from "./pages/Test";
+import App from "./App";
 import "./App.css";
 //import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Test />
+    <App />
   </React.StrictMode>
 );
